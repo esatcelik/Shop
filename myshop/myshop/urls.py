619d9fc2c554dd2e94703ae2f6ab1af0f90a8508
esadcelik <billwild00@gmail.com>
2014-11-10 06:36:36 +0200

    Signed-off-by: esadcelik <billwild00@gmail.com>
    home app and linking home page

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myshop.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'home.views.index'),
     url(r'^products/',include('products.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
