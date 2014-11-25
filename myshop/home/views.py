9c2716dc806e809bf65c3a543a37d87b182568e9
esadcelik <billwild00@gmail.com>
2014-11-25 12:43:25 +0200

    Signed-off-by: esadcelik <billwild00@gmail.com>
    Home page "latest" added

--- myshop/home/views.py
+++ myshop/home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from products.models import Bows, Arrows, Accessories
 
 # Create your views here.
 def main(request):
-    return render_to_response('index.html', RequestContext(request, {'user':request.user}))
+    b = Bows.objects.order_by('-pk')[:4]  # @UndefinedVariable
+    ar = Arrows.objects.order_by('-pk')[:4]  # @UndefinedVariable
+    ac = Accessories.objects.order_by('-pk')[:4]  # @UndefinedVariable
+    return render_to_response('index.html', RequestContext(request, {'user':request.user,'bows':b,'arrows':ar,'accessories':ac}))
 
 def contact(request):
     return render_to_response('contact.html', RequestContext(request, {'user':request.user}))
